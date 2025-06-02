5508a547e1e0c332a431afeaec683f0c068df220
Andrew Xie <dev@xie.is>
2025-06-02 11:25:59 -0700

    Add support for RouterPlugin

---
+++ src/test/projects/basic-router-plugin/src/main/java/its/BasicRouterPlugin.java
@@ -0,0 +1,12 @@
+package its;
+
+import com.facebook.presto.spi.RouterPlugin;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class BasicRouterPlugin
+        implements RouterPlugin
+{
+}
